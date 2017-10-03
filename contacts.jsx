f82f824d9a91fa82f2100600d4e3bde6c582abbe
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2017-10-03 14:49:13 +1100

    Styling for contacts page screens < 320px

--- contacts.jsx
+++ contacts.jsx
@@ -1,12 +1,4 @@
-// import {TopMenu} from './menu-block';
 
-// class Socialmedia extends React.Component {
-//     render() { 
-//         let socialmedia = this.props.socmedia;
-//         return (
-           
-//         );
-// }
 class Address extends React.Component { 
     render() {
         // let map = <iframe src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4938872.778644381!2d119.7591081942533!3d-66.48404922171159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sau!4v1506906423581' style={{ width = '600', height='450', frameBorder='0', style='border:0' }} allowFullScreen/>;
